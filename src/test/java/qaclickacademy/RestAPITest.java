2a233ae0e0fed6a76de842a82399b11278fed1be
raveena <raveena1712@gmail.com>
2020-09-18 13:22:15 +1200

    first commit

---
+++ src/test/java/qaclickacademy/RestAPITest.java
@@ -0,0 +1,18 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+    @Test
+    public  void Postjira()
+    {
+        System.out.println("Postjira");
+
+    }
+    @Test
+    public void deleteTwitter()
+    {
+        System.out.println("deleteTwitter");
+
+    }
+}
---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,21 @@
+package qaclickacademy;
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+    //testng xml file from maven
+    //how to execut all test cases from Test folder with help of maven test-mvn test
+    @Test
+    public void BrowserAutomation()
+    {
+        System.out.println("BrowserAutomation");
+
+    }
+    @Test
+    public void elementsUi()
+    {
+        System.out.println("elementsUi");
+
+    }
+}
